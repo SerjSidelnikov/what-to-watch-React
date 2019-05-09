86c5fe08eef44f006b5c32cd8bbe2d29fb870e71
SerjSidelnikov <serj2107@gmail.com>
2019-05-09 17:01:26 +0300

    Added mock files

---
+++ src/moks/films.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    title: `Fantastic Beasts`,
+    src: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+  },
+  {
+    title: `Bohemian Rhapsody`,
+    src: `img/bohemian-rhapsody.jpg`,
+  },
+  {
+    title: `Macbeth`,
+    src: `img/macbeth.jpg`,
+  },
+  {
+    title: `Aviator`,
+    src: `img/aviator.jpg`,
+  },
+  {
+    title: `We need to talk about Kevin`,
+    src: `img/we-need-to-talk-about-kevin.jpg`,
+  },
+];
