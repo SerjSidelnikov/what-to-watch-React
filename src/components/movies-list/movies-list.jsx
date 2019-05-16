8613f22c156378bb11c6380d9ec3912d3238b4ad
SerjSidelnikov <serj2107@gmail.com>
2019-05-16 10:48:49 +0300

    Made a VideoPlayer component

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -1,45 +1,26 @@
-import {PureComponent} from 'react';
-
 import MovieCard from '../movie-card/movie-card';
 
-class MoviesList extends PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      activeMovie: null,
-    };
-  }
-
-  handleActiveMovie(movie) {
-    this.setState({
-      activeMovie: movie,
-    });
-  }
-
-  render() {
-    const {movies, onClick} = this.props;
-
-    return (
-      <div className="catalog__movies-list">
-        {movies.map((movie, index) => (
-          <MovieCard
-            key={`movie-${index}`}
-            title={movie.title}
-            src={movie.src}
-            onClick={onClick}
-            onActiveMovie={this.handleActiveMovie.bind(this, movie)}
-          />
-        ))}
-      </div>
-    );
-  }
-}
+const MoviesList = ({movies, onClick}) => {
+  return (
+    <div className="catalog__movies-list">
+      {movies.map((movie, index) => (
+        <MovieCard
+          key={`movie-${index}`}
+          title={movie.title}
+          src={movie.src}
+          poster={movie.poster}
+          onClick={onClick}
+        />
+      ))}
+    </div>
+  );
+};
 
 MoviesList.propTypes = {
   movies: PropTypes.arrayOf(PropTypes.shape({
     title: PropTypes.string.isRequired,
     src: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
   })).isRequired,
   onClick: PropTypes.func.isRequired,
 };
---
+++ src/components/video-player/video-player.jsx
@@ -0,0 +1,78 @@
+import {PureComponent} from 'react';
+
+class VideoPlayer extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this._videoRef = React.createRef();
+
+    this.state = {
+      progress: 0,
+      isLoading: true,
+      isPlaying: props.isPlaying,
+    };
+  }
+
+  componentDidMount() {
+    const {src, poster, muted} = this.props;
+    const video = this._videoRef.current;
+
+    video.src = src;
+    video.poster = poster;
+    video.muted = muted;
+
+    video.oncanplaythrough = () => {
+      this.setState({isLoading: false});
+    };
+
+    video.onplay = () => {
+      this.setState({isPlaying: true});
+    };
+
+    video.onpause = () => {
+      this.setState({isPlaying: false});
+      video.load();
+    };
+
+    video.ontimeupdate = () => {
+      this.setState({progress: video.currentTime});
+    };
+  }
+
+  componentDidUpdate() {
+    const video = this._videoRef.current;
+
+    if (this.props.isPlaying) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  }
+
+  componentWillUnmount() {
+    const video = this._videoRef.current;
+
+    video.oncanplaythrough = null;
+    video.onplay = null;
+    video.onpause = null;
+    video.ontimeupdate = null;
+    video.src = ``;
+  }
+
+  render() {
+    return (
+      <video
+        ref={this._videoRef}
+      />
+    );
+  }
+}
+
+VideoPlayer.propTypes = {
+  src: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  muted: PropTypes.bool.isRequired,
+  isPlaying: PropTypes.bool.isRequired,
+};
+
+export default VideoPlayer;
