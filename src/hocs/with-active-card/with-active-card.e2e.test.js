d40d2c49aaee83024480d8d5ca0d5be47402b85b
SerjSidelnikov <serj2107@gmail.com>
2019-05-25 19:08:28 +0300

    Wrote tests

---
+++ src/hocs/with-active-card/with-active-card.e2e.test.js
@@ -0,0 +1,30 @@
+import {shallow} from 'enzyme';
+
+import withActiveCard from './with-active-card';
+
+const MockComponent = () => <div/>;
+const MockComponentWrapped = withActiveCard(MockComponent);
+
+describe(`withActiveCard`, () => {
+  it(`When you hover the cursor on the card plays video.`, () => {
+    jest.useFakeTimers();
+
+    const wrapper = shallow(<MockComponentWrapped/>);
+
+    expect(wrapper.props().isPlaying).toEqual(false);
+
+    wrapper.props().onMouseEnter();
+    jest.advanceTimersByTime(1000);
+
+    expect(wrapper.props().isPlaying).toEqual(true);
+    jest.useRealTimers();
+  });
+
+  it(`When you move the cursor from the card, the video stops.`, () => {
+    const wrapper = shallow(<MockComponentWrapped/>);
+
+    wrapper.props().onMouseLeave();
+
+    expect(wrapper.props().isPlaying).toEqual(false);
+  });
+});
